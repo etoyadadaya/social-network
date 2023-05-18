4e0816bc30e4859b72a4da422c7cf1f48ef35e12
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-05-18 17:47:59 +0700

    routing, code splitting, lazy, suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss';
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            ABOBA
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
