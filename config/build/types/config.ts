ccacdb5e49943eca3b9975b2ebe85c9f2f187981
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-05-19 14:30:03 +0700

    feature-sliced design architecture

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -9,6 +9,7 @@ export type BuildPaths = {
     entry: string;
     build: string;
     html: string;
+    src: string;
 }
 
 export interface BuildOptions {
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
 
 const App = () => {
 
@@ -18,8 +18,8 @@ const App = () => {
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/'} element={<MainPageAsync />}/>
-                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPage />}/>
+                    <Route path={'/about'} element={<AboutPage />}/>
                 </Routes>
             </Suspense>
         </div>
