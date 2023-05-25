c5e34bbefa20f56b99440d53ee625229288373d6
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-05-25 16:06:40 +0700

    sidebar, application layout

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 import React from "react";
 
 interface NavBarProps {
@@ -10,8 +9,7 @@ interface NavBarProps {
 
 export const Navbar = ({className}: NavBarProps) => {
     return (
-        <div className={classNames(cls.navbar)}>
-            <ThemeSwitcher />
+        <div className={classNames(cls.navbar, {}, [className])}>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
                     Главная
