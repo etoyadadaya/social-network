d129fe40ea839ed545a6b6ddd9b43b16cfc6eb71
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-05-26 16:09:19 +0700

    testing environment, configured jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,28 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        const expected = 'someClass';
+        expect(
+            classNames('someClass'),
+        ).toBe(expected);
+    });
+
+    test('with additional classes', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames(
+            'someClass',
+            {},
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+});
