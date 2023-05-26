1fad150ec9c00328fba5663f30c12d7b13c62bba
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-05-26 12:12:21 +0700

    little redesign in new colors

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import cls from "./Sidebar.module.scss";
-import { classNames } from "shared/lib/classNames/classNames";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
-import { LangSwitcher } from "widgets/LangSwitcher";
+import {classNames} from "shared/lib/classNames/classNames";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+import ArrowIcon from '../../../../shared/assets/icons/arrow.svg';
+import {Theme, useTheme} from "app/providers/ThemeProvider";
 
 interface SidebarProps {
     className?: string;
@@ -11,6 +14,7 @@ interface SidebarProps {
 export const Sidebar = ({className}: SidebarProps) => {
 
     const [collapsed, setCollapsed] = useState(false);
+    const { theme, toggleTheme } = useTheme();
 
     const onToggle = () => {
         setCollapsed(prev => !prev);
@@ -20,9 +24,20 @@ export const Sidebar = ({className}: SidebarProps) => {
         <div
             className={classNames(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
-            <button onClick={onToggle}>
-                toggle
-            </button>
+            <div className={cls.burgerSwitcher}>
+                <Button
+                    theme={ThemeButton.CLEAR}
+                    onClick={onToggle}
+                    className={collapsed ? cls.burgerCollapsed : cls.burgerNotCollapsed}
+                >
+                    { theme === Theme.DARK
+                        ?
+                        <ArrowIcon className={cls.ArrowIconDark} />
+                        :
+                        <ArrowIcon className={cls.ArrowIconLight} />
+                    }
+                </Button>
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
